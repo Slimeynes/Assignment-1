8b88381eba5e51d224f3c6422b3af2d917504398
FSD38\ryker.hutchinson <ryker.hutchinson@fsd38.ab.ca>
2020-09-15 09:13:34 -0600

    friday

---
+++ Assignment 01/Assets/Scripts/Player_controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour {
+    Rigidbody2D rb2d;
+
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Test player manager reporting!");
+        rb2d = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb2d.velocity = new Vector2(0,0);
+        if(Input.GetKey("w")) { 
+       rb2d.velocity = new Vector2(0, 10);
+        }
+        else if (Input.GetKey("s"))
+        {
+            rb2d.velocity = new Vector2(0, -10);
+        }
+        else if (Input.GetKey("a"))
+        {
+            rb2d.velocity = new Vector2(-10, 0);
+        }
+        else if (Input.GetKey("d"))
+        {
+            rb2d.velocity = new Vector2(10, 0);
+        }
+    }
+}

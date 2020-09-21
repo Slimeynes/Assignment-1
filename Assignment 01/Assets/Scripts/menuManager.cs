7e54d9e9d59e9cf1020f48d3162f4856f52ae7f7
FSD38\ryker.hutchinson <ryker.hutchinson@fsd38.ab.ca>
2020-09-21 11:12:24 -0600

    3rd week monday

--- Assignment 01/Assets/Scripts/menuManager.cs
+++ Assignment 01/Assets/Scripts/menuManager.cs
@@ -3,21 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class menuManager : MonoBehaviour {
-    public GameObject player;
-    public GameObject mainMenu;
+    public GameObject maintank;
+    public GameObject MainMenu;
+    public GameObject Cursor;
+    public GameObject[] CursorPositions;
+    public GameObject playerSelectIndicator;
+
+    private bool onStartGameSelection;
+
+    //int thisNumber = 0;
+    //float thisDecimalNumber = 0.5f;
+    //string yourName = "Harry Potter";
+    //bool yourValue = true; (or false)
+
+
+
+
     // Use this for initialization
     void Start () {
-		
-	}
+        onStartGameSelection = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey("Space") || Input.GetKey("return"))
+        if (Input.GetKey("space") || Input.GetKey("return"))
         {
-            player.SetActive(true);
-            mainMenu.SetActive(false);
+            maintank.SetActive(true);
+            MainMenu.SetActive(false);
 
         }
 
-	}
+        if (Input.GetKeyDown("up") || Input.GetKeyDown("down"))
+        {
+            if (onStartGameSelection)
+            {
+                onStartGameSelection = false;
+            }
+            else
+            {
+                onStartGameSelection = true;
+            }
+        }
+
+        if(onStartGameSelection)
+        {
+            Cursor.transform.position = CursorPositions[1].transform.position;
+        }
+        else
+        {
+            Cursor.transform.position = CursorPositions[0].transform.position;
+        }
+    }
 }

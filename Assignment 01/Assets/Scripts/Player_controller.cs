800d645ce23e22955d0b5e88177c65d641c5af60
FSD38\ryker.hutchinson <ryker.hutchinson@fsd38.ab.ca>
2020-09-30 12:01:40 -0600

    week 4 monday - wednesday bug fix

--- Assignment 01/Assets/Scripts/Player_controller.cs
+++ Assignment 01/Assets/Scripts/Player_controller.cs
@@ -27,75 +27,80 @@ public class Player_controller : MonoBehaviour {
             spriteRen.flipY = false;
             tankAnim.Play("basicTankUp");
        rb2d.velocity = new Vector2(0, 5);
+            playerDirection = tankDirection.up;
         }
         else if (Input.GetKey("s"))
         {
             rb2d.velocity = new Vector2(0, -5);
             spriteRen.flipY = true;
             tankAnim.Play("basicTankUp");
-            
+            playerDirection = tankDirection.down;
+
         }
         else if (Input.GetKey("a"))
         {
             rb2d.velocity = new Vector2(-5, 0);
             spriteRen.flipX = true;
             tankAnim.Play("basicTankRight");
-            
+            playerDirection = tankDirection.left;
+
         }
         else if (Input.GetKey("d"))
         {
             rb2d.velocity = new Vector2(5, 0);
            spriteRen.flipX = false;
             tankAnim.Play("basicTankRight");
+            playerDirection = tankDirection.right;
         }
+        else
+        {
+            rb2d.velocity = new Vector2(0, 0);
+        }
+
         if (Input.GetKeyDown("space"))
         {
-            Debug.Log("player tries fire");
+            // Debug.Log("player tries fire");
+            switch (playerDirection)
+            {
+                case tankDirection.up:
+                    // Fire up
+                    // Instantiate the projectile from a prefab!
+                    GameObject thisProjectile = Instantiate(basicBullet, transform.position, Quaternion.identity);
+                    Rigidbody2D projectileRB = thisProjectile.GetComponent<Rigidbody2D>();
+                    projectileRB.velocity = new Vector2(0, 10);
+                    break;
 
-            // Instantiate the player from a prefab!
-            GameObject thisProjectile = Instantiate(basicBullet, transform.position, Quaternion.identity);
-            Rigidbody2D projectileRB = thisProjectile.GetComponent<Rigidbody2D>();
-            projectileRB.velocity = new Vector2(0, 10);
-        }
+                case tankDirection.down:
+                    // Fire Down
+                    // Instantiate the projectile from a prefab!
+                    GameObject thisProjectileDown = Instantiate(basicBullet, transform.position, Quaternion.Euler(0f, 0f, 180f));
+                    Rigidbody2D projectileRBDown = thisProjectileDown.GetComponent<Rigidbody2D>();
+                    projectileRBDown.velocity = new Vector2(0, -10);
+                    break;
 
-        switch (playerDirection)
-        {
-            case tankDirection.up:
-                // Fire up
-                // Instantiate the projectile from a prefab!
-                GameObject thisProjectile = Instantiate(basicBullet, transform.position, Quaternion.identity);
-                Rigidbody2D projectileRB = thisProjectile.GetComponent<Rigidbody2D>();
-                projectileRB.velocity = new Vector2(0, 10);
-                break;
+                case tankDirection.right:
+                    // Fire right
+                    // Instantiate the projectile from a prefab!
+                    GameObject thisProjectileRight = Instantiate(basicBullet, transform.position, Quaternion.Euler(0f, 0f, -90f));
+                    Rigidbody2D projectileRBRight = thisProjectileRight.GetComponent<Rigidbody2D>();
+                    projectileRBRight.velocity = new Vector2(10, 0);
+                    break;
+                case tankDirection.left:
+                    // Fire left
+                    // Instantiate the projectile from a prefab!
+                    GameObject thisProjectileLeft = Instantiate(basicBullet, transform.position, Quaternion.Euler(0f, 0f, 90f));
+                    Rigidbody2D projectileRBLeft = thisProjectileLeft.GetComponent<Rigidbody2D>();
+                    projectileRBLeft.velocity = new Vector2(-10, 0);
+                    break;
 
-            case tankDirection.down:
-                // Fire Down
-                // Instantiate the projectile from a prefab!
-                GameObject thisProjectileDown = Instantiate(basicBullet, transform.position, Quaternion.Euler(0f, 0f, 180f));
-                Rigidbody2D projectileRBDown = thisProjectileDown.GetComponent<Rigidbody2D>();
-                projectileRBDown.velocity = new Vector2(0, -10);
-                break;
+                default:
+                    Debug.Log("playerDirection was invalid");
+                    break;
+            }
 
-            case tankDirection.right:
-                // Fire right
-                // Instantiate the projectile from a prefab!
-                GameObject thisProjectileRight = Instantiate(basicBullet, transform.position, Quaternion.Euler(0f, 0f, -90f));
-                Rigidbody2D projectileRBRight = thisProjectileRight.GetComponent<Rigidbody2D>();
-                projectileRBRight.velocity = new Vector2(10, 0);
-                break;
-            case tankDirection.left:
-                // Fire left
-                // Instantiate the projectile from a prefab!
-                GameObject thisProjectileLeft = Instantiate(basicBullet, transform.position, Quaternion.Euler(0f, 0f, 90f));
-                Rigidbody2D projectileRBLeft = thisProjectileLeft.GetComponent<Rigidbody2D>();
-                projectileRBLeft.velocity = new Vector2(-10, 0);
-                break;
-
-            default:
-                Debug.Log("playerDirection was invalid");
-                break;
         }
     }
+
     public enum tankDirection
     {
         up = 1,
@@ -103,6 +108,8 @@ public class Player_controller : MonoBehaviour {
         right,
         left,
     }
+
+
 }
 
 
